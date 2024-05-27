95b387a5bb650c71f4056ff959cf46db32a3994a
Ayushman choubey <choubey.ayushman13@gmail.com>
2024-05-27 22:44:37 +0530

    New Commit

---
+++ src/pages/Success/Success.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Success.css'
+
+const Success = () => {
+  return (
+    <div className='Order-confirm'>
+        <h2>Order Successful!</h2>
+        <p>Your order has been placed successfully</p>
+      
+    </div>
+  )
+}
+
+export default Success
